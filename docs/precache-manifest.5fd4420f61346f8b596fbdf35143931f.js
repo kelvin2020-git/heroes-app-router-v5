201da8b6e7ee4955a0f7e62729df58f9dea34ff0
kelvin <kelvincastillo429@gmail.com>
2022-04-11 08:08:36 -0600

    docs modificado

---
+++ docs/precache-manifest.5fd4420f61346f8b596fbdf35143931f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff9950774d97c7d6763666925cef494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8b1bdbb3a135f06211e",
+    "url": "/static/js/2.f5355828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f5355828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0125e43f7ee92d2f0a08",
+    "url": "/static/js/main.c9d80c93.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
